ad4aede4d1ab9673096b60ef29cccd3ca55ada8e
Mandijol <amykajol30@gmail.com>
2022-05-21 15:29:15 +0000

    Compare Numbers Using If statement
    Signed-off-by: Mandijol <amykajol30@gmail.com>

---
+++ compare_numbers.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#https://github.com/Mandijol/Bash_Script_beginner/blob/main/numbersscript.bash
+
+echo 'Enter first number'
+#accept first inputfrom user
+read a #stores first input in variable a
+
+echo 'Enter second number'
+#read second input
+read b   #stores second input in variable b
+
+if (($a<$b))                 #checks if value of a is less than value of b
+then
+ echo 'A is less than B'     #prints results to user
+ exit 1                      #ends program execution
+ fi
+
+if (($a>$b))                 #checks if value of  a is greater than value of b
+then
+echo 'A is greater than B'   #prints results to user
+ exit 1                      #ends the program execution
+fi
+
+if (($a==$b))                #checks if the value of a is equal to value of b
+then
+echo 'A is equal to B'      #prints the results to the user
+ exit 1            
+ fi
+ 
